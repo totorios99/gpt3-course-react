ef2d17de90501565449df0bcfe02363f4ba73b4a
Toto Rios <joseantonio_ra@live.com>
2022-03-07 00:32:21 -0600

    Completed header without media queries

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// Importando los estilos
+import './article.css';
+
+const Article = () => {
+  return (
+    <div>
+        Article
+    </div>
+  )
+}
+
+export default Article
